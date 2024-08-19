80e9470d1ca4ee5e0c341d73c967b97a70b4c7c4
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-19 17:39:57 +0200

    Pass all tests
    Created function that figure out all 7 tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if(times < 0) 
+        return "ERROR"
+    let returnValue = '';
+    for(i = 0; i < times; i++){
+        returnValue += word;
+    }
+    return returnValue;
 };
 
 // Do not edit below this line
