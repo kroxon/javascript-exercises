783a7523b906614ab9624f132026a81fb81103a8
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-19 19:37:32 +0200

    Pass alle 8 tests in exercise no. 4
    Create function that put only right value to new array. it's longer solution that Odin's.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array = [], ...args) {
+    newArray = [];
+    const arrayLength = array.length;
+    for (i = 0; i < arrayLength; i++) {
+        let element = array.pop();
+        if (!args.includes(element))
+            newArray.unshift(element)
+    }
+    return newArray;
 };
 
 // Do not edit below this line
