86a420beb17ddb07561a304ffd1acf3eb27e4e25
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-19 17:59:01 +0200

    Pass all test in exercise no. 3
    Created function that return reverse string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words = "") {
+    revereWords = ""
+    for (i = 0; i < words.length; i++) {
+        revereWords += words.charAt(words.length - 1 - i)
+    }
+    return revereWords
 };
 
 // Do not edit below this line
