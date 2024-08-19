1b498cf8b108ee4a6c617e00b1ecf42581aefc36
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-19 19:58:16 +0200

    Pass 7 tests
    Created function that check if received numbers are integer type, check which number is greater and sum all numbers from range.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+    try {
+        if (firstNumber < 1 || secondNumber < 1 || !Number.isInteger(firstNumber) || !Number.isInteger(secondNumber))
+            return "ERROR"
+        if (firstNumber > secondNumber) {
+            let temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+        }
+        sum = 0
+        for (i = firstNumber; i <= secondNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    } catch (error) {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
