aa59c5c8f9ba2ec4663bb2c1d6acfe0dbedfd331
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-26 17:47:34 +0200

    Solve palindrome problem
    Created function that check if string if palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+function clearText(string) {
+    return Array.from(string)
+        .filter(symbol => (/[a-zA-Z0-9]/).test(symbol))
+        .map(symbol => {
+            if ((/[a-zA-Z]/).test(symbol))
+                return symbol.toLowerCase();
+            else
+                return symbol;
+        });
+}
 
+function reverseString(array) {
+    const tempArray = array.slice();
+    for (i = 0; i < array.length; i++) {
+        tempArray[i] = array[array.length - 1 - i];
+    }
+    return tempArray;
+}
+
+const palindromes = function (string) {
+    return clearText(string).join('') == reverseString(clearText(string)).join('');
 };
 
 // Do not edit below this line
