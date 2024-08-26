6ef27b6179cf1df1ea0105fc2f4dd0c4ba5afb33
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-26 20:28:33 +0200

    Find the oldest person
    Created compared by age property function and sorted array of people.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function ageCompare(a, b) {
+    if (!a.yearOfDeath)
+        a.yearOfDeath = (new Date).getFullYear();
+    if (!b.yearOfDeath)
+        b.yearOfDeath = (new Date).getFullYear();
+    const age1 = a.yearOfDeath - a.yearOfBirth;
+    const age2 = b.yearOfDeath - b.yearOfBirth;
+    return age1 - age2;
+}
 
+const findTheOldest = function (array) {
+    const oldest = array.sort(ageCompare);
+    return oldest[oldest.length - 1];
 };
 
 // Do not edit below this line
